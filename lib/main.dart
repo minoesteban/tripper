13e8ef2b47a029fe3b8bd36a96a1582cad426ad7
esteban miño <minoesteban1@gmail.com>
2024-01-23 11:46:49 -0300

    l10n main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tripper/l10n/l10n_utils.dart';
 
-void main() {
+void main() async {
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: TripperApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TripperApp extends StatelessWidget {
+  const TripperApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: L10n.current.appName,
+      localizationsDelegates: const [
+        L10n.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
